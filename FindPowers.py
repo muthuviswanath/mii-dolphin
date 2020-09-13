83f986585c83f9df24ddc64dfe6e2a845ef65e24
muthuviswanath <44441145+muthuviswanath@users.noreply.github.com>
2020-09-13 15:39:58 +0530

    Update and rename Find-Squares to FindPowers.py

---
+++ FindPowers.py
@@ -0,0 +1,4 @@
+num =int(input("Enter the number to find its square: "))
+power = int(input("Enter the power the number has to raised to: "))
+result = num ** power
+print(f"Square of the number {num} is: {result}")
